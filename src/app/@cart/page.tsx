4a58185f3240d568859a74d255ce01da76c70312
OndrejHj04 <ondra.hajku@seznam.cz>
2024-04-25 23:23:41 +0200

    feat(cart): display cart price and count

--- src/app/@cart/page.tsx
+++ src/app/@cart/page.tsx
@@ -1,14 +1,20 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { store } from "@/lib/store";
+import { calculateTickets } from "@/lib/utils";
 
 export default function Cart() {
+  const { tickets } = store();
+  const { count, price } = calculateTickets(tickets);
+
   return (
     <nav className="sticky bottom-0 left-0 right-0 bg-white border-t border-zinc-200 flex justify-center">
       {/* inner content */}
       <div className="max-w-screen-lg p-6 flex justify-between items-center gap-4 grow">
         {/* total in cart state */}
         <div className="flex flex-col">
-          <span>Total for [?] tickets</span>
-          <span className="text-2xl font-semibold">[?] CZK</span>
+          <span>Total for {count} tickets</span>
+          <span className="text-2xl font-semibold">{price} CZK</span>
         </div>
 
         {/* checkout button */}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { ticketDetail } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function calculateTickets(tickets: ticketDetail[]) {
+  return {
+    price: tickets.reduce((a, b) => a + b.price, 0),
+    count: tickets.length,
+  };
 }
